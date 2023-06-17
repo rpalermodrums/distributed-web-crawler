2f8f54a2ee03c014fe517b39873f77271ec2a332
Ryan  Palermo <rpalermodrums@gmail.com>
2023-06-16 22:38:08 -0400

    update to save output to csv

--- README.md
+++ README.md
@@ -9,17 +9,20 @@ This project requires Python 3 and the following Python libraries installed:
 - BeautifulSoup
 - requests
 - urllib
+- csv
+
 
 If Python is not installed on your machine, you can install it from the official website - https://www.python.org/.
 
-You can install BeautifulSoup, requests, and urllib using pip:
+You can install BeautifulSoup, requests, urllib, and csv using pip:
 
 
 ```
-pip install beautifulsoup4 requests urllib
+pip install beautifulsoup4 requests urllib 
 
 ```
 
+The `csv` module comes pre-installed with Python, so you don't need to install it separately.
 ## Usage
 
 You can run the script using Python:
@@ -29,16 +32,17 @@ python crawler.py https://www.example.com/ --depth 2
 
 ```
 
-
 Replace 'https://www.example.com/' with the URL you want to crawl. The script will print all links found on the webpage and follow these links to a depth specified by the `--depth` argument. If `--depth` is not provided, it defaults to 2.
 
 The crawler respects the rules set out in the `robots.txt` file of the website. If the `robots.txt` file disallows crawling a particular page, the crawler will skip that page. The crawler also waits 1 second between requests to rate limit itself.
 
 Errors encountered during the crawl, such as network errors or parsing errors, will be printed to the console, but the crawler will attempt to continue with the next URL.
 
+The `--output` argument specifies the output CSV file where the URL and title of visited pages will be stored. If `--output` is not provided, it defaults to `output.csv`.
+
 ## Limitations
 
-This web crawler does not handle dynamic content loaded with JavaScript. It's a basic tool for educational purposes and should be used responsibly. 
+This web crawler does not handle dynamic content loaded with JavaScript. It's a basic tool for educational purposes and should be used responsibly.
 
 ## License
 
--- crawler.py
+++ crawler.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 import argparse
 import urllib.parse
 import time
+import csv
 from urllib.robotparser import RobotFileParser
 
-def crawl(url, depth):
+def crawl(url, depth, output_file):
     visited = set()
     to_visit = {(url, 0)} # (url, depth)
 
@@ -17,48 +18,55 @@ def crawl(url, depth):
         print(f"Error reading robots.txt: {e}")
         return
 
-    while to_visit:
-        current_url, current_depth = to_visit.pop()
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['URL', 'Title'])
 
-        if current_depth > depth or not rp.can_fetch("*", current_url):
-            continue
+        while to_visit:
+            current_url, current_depth = to_visit.pop()
 
-        try:
-            response = requests.get(current_url, timeout=5)
-            response.raise_for_status()
-        except (requests.HTTPError, requests.ConnectionError) as e:
-            print(f"Error fetching {current_url}: {e}")
-            continue
-
-        try:
-            soup = BeautifulSoup(response.text, 'html.parser')
-        except Exception as e:
-            print(f"Error parsing {current_url}: {e}")
-            continue
-
-        visited.add(current_url)
-        print(current_url)
-
-        for link in soup.find_all('a'):
-            href = link.get('href')
-
-            # Ignore mailto links and telephone links
-            if href and (href.startswith(('mailto:', 'tel:', 'fax:', 'file:', 'data:', 'sms:', 'news:', 'irc:', 'magnet:'))):
+            if current_depth > depth or not rp.can_fetch("*", current_url):
                 continue
 
-            # Resolve relative links
-            if href and not href.startswith(('http://', 'https://')):
-                href = urllib.parse.urljoin(current_url, href)
+            try:
+                response = requests.get(current_url, timeout=5)
+                response.raise_for_status()
+            except (requests.HTTPError, requests.ConnectionError) as e:
+                print(f"Error fetching {current_url}: {e}")
+                continue
 
-            if href not in visited:
-                to_visit.add((href, current_depth + 1))
+            try:
+                soup = BeautifulSoup(response.text, 'html.parser')
+            except Exception as e:
+                print(f"Error parsing {current_url}: {e}")
+                continue
 
-        time.sleep(1)  # Delay between requests
+            visited.add(current_url)
+
+            title = soup.title.string if soup.title else 'No title'
+            writer.writerow([current_url, title])
+
+            for link in soup.find_all('a'):
+                href = link.get('href')
+
+                # Ignore mailto links and telephone links
+                if href and (href.startswith(('mailto:', 'tel:', 'fax:', 'file:', 'data:', 'sms:', 'news:', 'irc:', 'magnet:'))):
+                    continue
+
+                # Resolve relative links
+                if href and not href.startswith(('http://', 'https://')):
+                    href = urllib.parse.urljoin(current_url, href)
+
+                if href not in visited:
+                    to_visit.add((href, current_depth + 1))
+
+            time.sleep(1)  # Delay between requests
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='A web crawler that respects robots.txt rules and rate limits requests.')
+    parser = argparse.ArgumentParser(description='A web crawler that respects robots.txt rules, rate limits requests, and stores the URL and title of visited pages.')
     parser.add_argument('url', help='The URL to crawl.')
     parser.add_argument('--depth', type=int, default=2, help='The maximum depth to crawl.')
+    parser.add_argument('--output', default='output.csv', help='The output file where the URL and title of visited pages will be stored.')
     args = parser.parse_args()
 
-    crawl(args.url, args.depth)
+    crawl(args.url, args.depth, args.output)
