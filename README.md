97faa682190b1b1cb0ce3bbbcab844edddfe491e
Ryan  Palermo <rpalermodrums@gmail.com>
2023-06-16 23:18:49 -0400

    add setup for concurrency and multi-threading

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Web Crawler CLI
 
-A web crawler that respects `robots.txt` rules, rate limits requests, and handles errors gracefully. It starts from a seed URL and follows links to a specified depth.
+A web crawler that respects `robots.txt` rules, rate limits requests, handles errors gracefully, stores the URL and title of visited pages in a CSV file, logs its activity to a file, and uses multiple threads to download pages concurrently. It starts from a seed URL and follows links to a specified depth.
 
 ## Installation
 
@@ -10,27 +10,26 @@ This project requires Python 3 and the following Python libraries installed:
 - requests
 - urllib
 - csv
-
+- concurrent.futures
 
 If Python is not installed on your machine, you can install it from the official website - https://www.python.org/.
 
-You can install BeautifulSoup, requests, urllib, and csv using pip:
-
-
-```
-pip install beautifulsoup4 requests urllib 
+You can install BeautifulSoup and requests using pip:
 
 ```
+pip install beautifulsoup4 requests
+```
 
-The `csv` module comes pre-installed with Python, so you don't need to install it separately.
+The `csv`, `concurrent.futures`, and `urllib` modules come pre-installed with Python, so you don't need to install them separately.
+
 ## Usage
 
 You can run the script using Python:
 
 ```
-python crawler.py https://www.example.com/ --depth 2
-
+python crawler.py https://www.example.com/ --depth 2 --output output.csv
 ```
+
 
 Replace 'https://www.example.com/' with the URL you want to crawl. The script will print all links found on the webpage and follow these links to a depth specified by the `--depth` argument. If `--depth` is not provided, it defaults to 2.
 
@@ -42,10 +41,13 @@ The `--output` argument specifies the output CSV file where the URL and title of
 
 The crawler logs its activity to a file named `web_crawler.log`. This includes the URLs it visits, any errors it encounters, and other information.
 
+The crawler uses multiple threads to download pages concurrently. This can make it faster for larger websites, but the order of URLs in the output file will not necessarily match the order in which they were discovered or visited.
+
 ## Limitations
 
-This web crawler does not handle dynamic content loaded with JavaScript. It's a basic tool for educational purposes and should be used responsibly. 
+This web crawler does not handle dynamic content loaded with JavaScript. It's a basic tool for educational purposes and should be used responsibly.
 
 ## License
 
 This project is licensed under the MIT License - see the LICENSE.md file for details
+
--- crawler.py
+++ crawler.py
@@ -6,6 +6,8 @@ import time
 import csv
 import logging
 from urllib.robotparser import RobotFileParser
+import concurrent.futures
+import threading
 
 def setup_logger():
     logger = logging.getLogger('web_crawler')
@@ -17,9 +19,52 @@ def setup_logger():
     logger.addHandler(handler)
     return logger
 
+def process_url(url, depth, rp, visited, logger):
+    if url is None or not rp.can_fetch("*", url):
+        return [], 'No title'
+
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ConnectionError) as e:
+        print(f"Error fetching {url}: {e}, thread: {threading.current_thread().name}")
+        logger.error(f"Error fetching {url}: {e}, thread: {threading.current_thread().name}")
+        return [], 'No title'
+
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+    except Exception as e:
+        print(f"Error parsing {url}: {e}, thread: {threading.current_thread().name}")
+        logger.error(f"Error parsing {url}: {e}, thread: {threading.current_thread().name}")
+        return [], 'No title'
+
+    visited.add(url)
+
+    title = soup.title.string if soup.title else 'No title'
+    print(f"Crawled {url}, title: {title}, thread: {threading.current_thread().name}")
+    logger.info(f"Crawled {url}, title: {title}, thread: {threading.current_thread().name}")
+
+    new_links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+
+        # Ignore mailto links and telephone links
+        if href and (href.startswith(('mailto:', 'tel:', 'fax:', 'file:', 'data:', 'sms:', 'news:', 'irc:', 'magnet:'))):
+            continue
+
+        # Resolve relative links
+        if href and not href.startswith(('http://', 'https://')):
+            href = urllib.parse.urljoin(url, href)
+
+        if href and href not in visited:
+            new_links.append(href)
+
+    time.sleep(1)  # Delay between requests
+    return new_links, title
+
 def crawl(url, depth, output_file):
     visited = set()
-    to_visit = {(url, 0)} # (url, depth)
+    to_visit = [(url, 0)] # (url, depth)
     logger = setup_logger()
 
     rp = RobotFileParser()
@@ -27,57 +72,28 @@ def crawl(url, depth, output_file):
     try:
         rp.read()
     except Exception as e:
-        print(f"Error reading robots.txt: {e}")
-        logger.error(f"Error reading robots.txt: {e}")
+        print(f"Error reading robots.txt: {e}, thread: {threading.current_thread().name}")
+        logger.error(f"Error reading robots.txt: {e}, thread: {threading.current_thread().name}")
         return
 
     with open(output_file, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['URL', 'Title'])
 
-        while to_visit:
-            current_url, current_depth = to_visit.pop()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            while to_visit:
+                current_url, current_depth = to_visit.pop()
 
-            if current_depth > depth or current_url is None or not rp.can_fetch("*", current_url):
-                continue
-
-            try:
-                response = requests.get(current_url, timeout=5)
-                response.raise_for_status()
-            except (requests.HTTPError, requests.ConnectionError) as e:
-                print(f"Error fetching {current_url}: {e}")
-                logger.error(f"Error fetching {current_url}: {e}")
-                continue
-
-            try:
-                soup = BeautifulSoup(response.text, 'html.parser')
-            except Exception as e:
-                print(f"Error parsing {current_url}: {e}")
-                logger.error(f"Error parsing {current_url}: {e}")
-                continue
-
-            visited.add(current_url)
-
-            title = soup.title.string if soup.title else 'No title'
-            writer.writerow([current_url, title])
-            print(f"Crawled {current_url}, title: {title}")
-            logger.info(f"Crawled {current_url}, title: {title}")
-
-            for link in soup.find_all('a'):
-                href = link.get('href')
-
-                # Ignore mailto links and telephone links
-                if href and (href.startswith(('mailto:', 'tel:', 'fax:', 'file:', 'data:', 'sms:', 'news:', 'irc:', 'magnet:'))):
+                if current_depth > depth:
                     continue
 
-                # Resolve relative links
-                if href and not href.startswith(('http://', 'https://')):
-                    href = urllib.parse.urljoin(current_url, href)
+                future = executor.submit(process_url, current_url, current_depth, rp, visited, logger)
+                new_links, title = future.result()
 
-                if href and href not in visited:
-                    to_visit.add((href, current_depth + 1))
+                writer.writerow([current_url, title])
 
-            time.sleep(1)  # Delay between requests
+                for link in new_links:
+                    to_visit.append((link, current_depth + 1))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A web crawler that respects robots.txt rules, rate limits requests, and stores the URL and title of visited pages.')
